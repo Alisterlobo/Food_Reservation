365fb6a818ffc1a69432c92c7bb96f9ea609edcb
Alister-lobo <alisterlobo9483@gmail.com>
2025-05-01 17:05:36 +0530

    frontend  updated

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -17,12 +17,12 @@ const Reservation = () => {
 
         const handleReservation = async (e) => {
             e.preventDefault();
-            const API_BASE_URL = "https://food-reservation-i1cb.onrender.com";
+            // const API_BASE_URL = "https://food-reservation-i1cb.onrender.com";
 
             try{
-                const {data} = await axios.post(`${API_BASE_URL}/api/v1/reservation/send`,
+                // const {data} = await axios.post(`${API_BASE_URL}/api/v1/reservation/send`,
 
-                // const {data} = await axios.post("http://localhost:4000/api/v1/reservation/send",
+                const {data} = await axios.post("http://localhost:4000/api/v1/reservation/send",
                     {firstName, lastName, email, phone, date, time},
                     {
                         headers: {
