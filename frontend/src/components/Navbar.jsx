14d1021d033a5528437868db8e292624f7169b68
Alister-lobo <alisterlobo9483@gmail.com>
2025-05-02 13:16:18 +0530

    frontend updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <div className="logo">Alley</div>
+        <div className="logo">AlleyReserve</div>
         <div className={show ? "navLinks showmenu" : "navLinks"}>
           <div className="links">
             {data[0].navbarLinks.map((element) => (
